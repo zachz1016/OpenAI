97b91d607575dbbf4d1c1d60fded14c520b0cdd3
zachz1016 <xuechengzhang1016@gmail.com>
2023-03-08 10:24:19 +0800

    image generate

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -38,4 +38,39 @@ const generateText = async (req, res)=>{
     }
 };
 
-module.exports = {generateText}
+const generateImage = async (req, res) =>{
+
+    const { prompt, number, size} = req.body
+
+    const imageSize = size === 'small' ? '256x256' : size === 'medium'?
+    '512x512': '1024x1024'
+
+    try {
+        const response = await openai.createImage({
+            prompt: prompt,
+            n: number,
+            size: imageSize
+        });
+
+        const imageUrl = response.data.data[0].url
+
+        res.status(200).json({
+            success: true,
+            data: imageUrl
+        });
+    } catch (error) {
+    if (error.response) {
+        console.log(error.response.status);
+        console.log(error.response.data);
+    } else {
+        console.log(error.message);
+    }
+        res.status(400).json({
+        success: false,
+        error:'The image cannot be generated'
+        });
+    
+    }
+}
+
+module.exports = {generateText, generateImage}
--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {generateText} = require('../controllers/openaiController')
+const {generateText, generateImage} = require('../controllers/openaiController')
 
 router.post('/generateText', generateText);
+// router.post('/generateText', (req,res)=>{
+//     res.status(200).json({
+//         success: true,
+//     });
+// });
+router.post('/generateImage', generateImage);
 
 module.exports = router;
